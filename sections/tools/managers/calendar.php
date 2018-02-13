24331bcd7d4d1fd56230cca8794ae3f017de8687
Tomás Soler <tomas@tjsoler.me>
2018-02-13 12:11:17 +0100

    Move CalendarView into autoloader

--- sections/tools/managers/calendar.php
+++ sections/tools/managers/calendar.php
@@ -15,14 +15,14 @@ if (empty($Month) || empty($Year)) {
 $Events = \Gazelle\Calendar::get_events($Month, $Year);
 View::show_header("Calendar", "jquery.validate,form_validate,calendar", "calendar");
 
-CalendarView::render_title($Month, $Year);
+\Gazelle\CalendarView::render_title($Month, $Year);
 ?>
 <div class="sidebar">
 	<div id="event_div"></div>
 </div>
 <div class="main_column">
 <?
-	CalendarView::render_calendar($Month, $Year, $Events);
+	\Gazelle\CalendarView::render_calendar($Month, $Year, $Events);
 ?>
 </div>
 <?
